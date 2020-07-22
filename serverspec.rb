c51f51399950dce25f1b8d7d83237b257a70912d
takuya <takuya2926@gmail.com>
2020-07-22 20:45:16 +0900

    第１２回課題提出

---
+++ serverspec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+#ポート関係↓
+
+describe port(80) do
+  it { should be_listening }
+end
+
+describe port(22) do
+  it { should be_listening }
+end
+
+#describe port(443) do
+#  it { should be_listening }
+#end
+#httpsの環境構築をしていないのでエラー
+
+describe port(3000) do
+  it { should be_listening }
+end
+#railsサーバー起動時にtrue
+
+#パッケージ関係↓
+
+describe package('git') do
+  it { should be_installed }
+end
+
+describe command('ruby -v') do
+  its(:stdout) { should match /ruby 2\.7\.1/ }
+end
+
+describe command('rails -v') do
+  its(:stdout) { should match /Rails 6\.0\.3\.2/ }
+end
+
+describe package('mysql-community-server') do
+  it { should be_installed }
+end
+
+
+  
